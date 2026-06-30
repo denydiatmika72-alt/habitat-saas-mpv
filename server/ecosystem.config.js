75e598e1de69efb43060fe2d32e6a200ed4e12dd
Deny Diatmika <denydiatmika72@gmail.com>
2026-06-30 10:15:46 +0800

    fix: remove env override in ecosystem config, use dotenv only

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     name: 'nexevent-api',
     script: 'src/index.js',
     cwd: '/var/www/nexevent/server',
-    env: {
-      NODE_ENV: 'production',
-      ADMIN_EMAILS: 'denydiatmika72@gmail.com'
-    }
+    env_file: '/var/www/nexevent/server/.env'
   }]
 }
